12aa7aa62391388de76245239dff79ad05994fd7
Tran Duc Vinh <ducvinhtran22@gmail.com>
2022-05-08 21:38:48 +0700

    add discount

---
+++ discount.php
@@ -0,0 +1,46 @@
+<section class="py-5">
+    <div class="container">
+        <div class="card rounded-0">
+            <div class="card-body">
+                <p style="text-align: justify;">
+                    <span style="font-size:14px;"><span style="font-family:arial,helvetica,sans-serif;">QUYỀN LỢI VÀ ƯU ĐÃI</span></span>
+                </p>
+                <p style="text-align: justify;">
+                    <span style="font-size:14px;"><span style="font-family:arial,helvetica,sans-serif;"><strong>1. Dành cho tất cả khách hàng:</strong></span></span>
+                </p>
+                <p style="text-align: justify;">
+                    <span style="font-size:14px;"><span style="font-family:arial,helvetica,sans-serif;">- Giảm 15% tất cả các loại sách, truyện</span></span>
+                </p>
+                <p style="text-align: justify;">
+                    <span style="font-size:14px;"><span style="font-family:arial,helvetica,sans-serif;">- Thường xuyên có các chương trình khuyến mãi các ngành hàng từ 10 - 50% diễn ra hàng tháng trên nhasachminhthang.vn</span></span>
+                </p>
+                <p style="text-align: justify;">
+                    <span style="font-size:14px;"><span style="font-family:arial,helvetica,sans-serif;">- Nhận thiệp & quà mừng Sinh nhật</span></span>
+                </p>
+                <p style="text-align: justify;">
+                    <span style="font-size:14px;"><span style="font-family:arial,helvetica,sans-serif;">- Nhận được các chương trình, dịch vụ ưu đãi khác từ các đối tác của nhà sách</span></span>
+                </p>
+                <p style="text-align: justify;">
+                    <span style="font-size:14px;"><span style="font-family:arial,helvetica,sans-serif;">- Ngoài ra, Nhà sách Minh Thắng hiểu được tâm lý khách hàng là mong muốn giảm thiểu các khoản phí phát sinh, do đó chúng tôi hỗ trợ thêm cước phí vận chuyển cho mọi đơn hàng đối với Quý khách hàng liên tỉnh</span></span>
+                </p>
+                <p style="text-align: justify;">
+                    <span style="font-size:14px;"><span style="font-family:arial,helvetica,sans-serif;">- Thanh toán bằng tiền mặt: Từ 30 - 50% phí thu tiền hộ (COD).</span></span>
+                </p>
+                <p style="text-align: justify;">
+                    <span style="font-size:14px;"><span style="font-family:arial,helvetica,sans-serif;">Mong rằng Quý khách sẽ hài lòng khi mua hàng tại nhasachminhthang.vn.</span></span>
+                </p>
+                <p style="text-align: justify;">
+                </p>
+                <p style="text-align: justify;">
+                    <span style="font-size:14px;"><span style="font-family:arial,helvetica,sans-serif;"><strong>2. Dành cho Khách hàng thân thiết:</strong></span></span>
+                </p>
+                <p style="text-align: justify;">
+                    <span style="font-size:14px;"><span style="font-family:arial,helvetica,sans-serif;">Giảm giá 20% tất cả các loại Sách, truyện Tiếng Việt khi tổng hóa đơn tích lũy trong 12 tháng kể từ khi tạo tài khoản ≥ 02 triệu </span></span>
+                </p>
+                <p style="text-align: justify;">
+                    <span style="font-size:14px;"><span style="font-family:arial,helvetica,sans-serif;">Ngoài ra, Nhà sách Minh Thắng còn dành những phần quà đặc biệt cho Khách hàng thân thiết vào các dịp đặc biệt trong năm: Sinh nhật, Tết Nguyên Đán, Giáng sinh,…</span></span>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
